32d2f66193ac0c768118ebefc8e563ec765f1b75
unknown <garzonbrayan1201@hotmail.com>
2019-07-18 14:20:13 -0500

    configuración firebase y angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBALWbLzdbifExWFem7JvpzSpci2NwFHgA",
+    authDomain: "backlogv1.firebaseapp.com",
+    databaseURL: "https://backlogv1.firebaseio.com",
+    projectId: "backlogv1",
+    storageBucket: "",
+    messagingSenderId: "1067879412092",
+    appId: "1:1067879412092:web:38f4e0f735631aaf"
+  }
 };
