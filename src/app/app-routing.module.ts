f60297bbc5bb846582fc9641fafb1e61ddd157aa
unknown <garzonbrayan1201@hotmail.com>
2019-07-18 11:03:57 -0500

    Rutas de pantallas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{HomepageComponent} from './componentes/home-page/home-page.component';
+import{LoginpageComponent} from './componentes/login-page/login-page.component';
+import{RegisterpageComponent} from './componentes/register-page/register-page.component';
+import{PrivadopageComponent} from './componentes/privado-page/privado-page.component';
+import{NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomepageComponent},
+  {path:'login',component:LoginpageComponent},
+  {path:'register',component:RegisterpageComponent},
+  {path:'Privado',component:PrivadopageComponent},
+  {path:'**',component:NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './componentes/homepage/homepage.component';
+import { HomepageComponent } from './componentes/home-page/home-page.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
-import { RegisterpageComponent } from './componentes/registerpage/registerpage.component';
-import { LoginpageComponent } from './componentes/loginpage/loginpage.component';
-import { PrivadopageComponent } from './componentes/privadopage/privadopage.component';
+import { RegisterpageComponent } from './componentes/register-page/register-page.component';
+import { LoginpageComponent } from './componentes/login-page/login-page.component';
+import { PrivadopageComponent } from './componentes/privado-page/privado-page.component';
 import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 
